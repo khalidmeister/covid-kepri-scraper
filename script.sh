436f2d7bd34d47a6adb222bfe7bd6ebee78d5d63
Irfan Khalid <irfan_khalid@apps.ipb.ac.id>
2020-08-03 23:30:51 +0700

    Update Dataset

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# cd ~/projects/covid-kepri-scraper && /usr/bin/python kepri-scraper.py >> overall.out
+cd ~/projects/covid-kepri-scraper && /usr/bin/python kepri-scraper.py >> overall.out
 
 sleep 5
 
-cd ~/projects/covid-kepri-scraper && /usr/bin/git add .
-cd ~/projects/covid-kepri-scraper && /usr/bin/git commit -m "Update Dataset"
+cd ~/projects/covid-kepri-scraper && /usr/bin/git add . >> overall.out
+cd ~/projects/covid-kepri-scraper && /usr/bin/git commit -m "Update Dataset" >> overall.out
 HOME = /home/pi/.ssh/id_rsa
-/usr/bin/git push origin master
+/usr/bin/git push origin master >> overall.out
